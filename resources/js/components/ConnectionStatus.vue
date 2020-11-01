2210a8890fcf39351e068680b99765dc9309d3f8
Casper Boone <mail@casperboone.nl>
2020-11-01 16:07:31 +0100

    Indicate uncertainty of number of participants

--- resources/js/components/ConnectionStatus.vue
+++ resources/js/components/ConnectionStatus.vue
@@ -2,7 +2,7 @@
     <div class="bg-purple-100 p-2 rounded-md flex items-center">
         <div class="h-2 w-2 mr-2 rounded-full bg-purple-500"></div>
         <span class="text-purple-500 uppercase tracking-wider text-xs">
-            Connected <span v-if="showParticipants">({{ participants.length }} participants)</span>
+            Connected <span v-if="showParticipants">(~{{ participants.length }} participants)</span>
         </span>
     </div>
 </template>
