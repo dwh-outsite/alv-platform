2d9b908de0b9fb770c4d32ef090d79ff2d4f7d39
Casper Boone <mail@casperboone.nl>
2020-11-01 16:04:19 +0100

    Improve question form layout

--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="h-full flex p-4 text-gray-100 min-h-48">
+    <div class="h-full flex flex-col p-4 text-gray-100 min-h-48">
         <div
             v-if="state == 'submitted'"
-            class="flex-1 mr-4 form-input  border-0 shadow bg-green-800 flex items-center justify-center"
+            class="flex-1 mb-2 form-input border-0 shadow bg-green-800 flex items-center justify-center"
         >
             <div class="text-center">
                 <div class="font-bold">Bedankt voor je vraag!</div>
@@ -12,18 +12,17 @@
         <textarea
             v-if="state == 'available' || state == 'loading'"
             v-model="question"
-            class="flex-1 mr-4 form-input border-0 shadow bg-gray-800"
-            placeholder="Schrijf hier je vraag en klik daarna rechts op versturen ..."
+            class="flex-1 mb-2 form-input border-0 shadow bg-gray-800"
+            placeholder="Schrijf hier je vraag en klik daarna op versturen ..."
         />
         <button
             v-if="state == 'available' || state == 'loading'"
             @click="submit"
-            class="bg-gray-700 font-bold p-4 px-6 rounded leading-relaxed"
+            class="bg-gray-700 font-bold py-3 px-6 rounded leading-relaxed"
             :class="question != '' ? 'bg-purple-700 hover:bg-purple-500' : ''"
             :disabled="state !== 'available' || question == ''"
         >
-            Vraag<br />
-            Versturen
+            Vraag versturen
         </button>
     </div>
 </template>
