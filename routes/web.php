09e95c71b24832b711c3044002679faa4ec149e0
Casper Boone <mail@casperboone.nl>
2021-01-28 16:14:12 +0100

    Make registration routes publicly available

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ use Illuminate\Support\Facades\Route;
 Route::view('/login', 'login')->name('login');
 Route::post('/login', LoginController::class)->name('login-post');
 
+Route::view('/register', 'register')->name('register');
+Route::post('/register', RegisterController::class)->name('register-post');
+
 Route::middleware('auth')->group(function () {
     Route::redirect('/', 'live');
     Route::get('/live', LiveController::class)
@@ -63,9 +66,6 @@ Route::middleware('auth:admin')->group(function () {
 
         return ['token' => $token->plainTextToken];
     });
-
-    Route::view('/register', 'register')->name('register');
-    Route::post('/register', RegisterController::class)->name('register-post');
 });
 
 /**
