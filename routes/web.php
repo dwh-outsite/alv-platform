3fb3651605c9fd455fdb7fe77aad0a9645ef5d9b
Casper Boone <mail@casperboone.nl>
2021-04-20 16:08:58 +0200

    Temporarily disable registration

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/login', 'login')->name('login');
 Route::post('/login', LoginController::class)->name('login-post');
 
-Route::view('/register', 'register')->name('register');
-Route::post('/register', RegisterController::class)->name('register-post');
-
 Route::middleware('auth')->group(function () {
     Route::redirect('/', 'live');
     Route::get('/live', LiveController::class)
@@ -66,6 +63,10 @@ Route::middleware('auth:admin')->group(function () {
 
         return ['token' => $token->plainTextToken];
     });
+
+    // Temporarily make registration routes protected to disable registration after the deadline
+    Route::view('/register', 'register')->name('register');
+    Route::post('/register', RegisterController::class)->name('register-post');
 });
 
 /**
