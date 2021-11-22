b6eadc2c612af7be5fe71f8de72cc2d8448a9048
Casper Boone <mail@casperboone.nl>
2021-11-22 17:10:36 +0100

    Temporarily disable registration routes

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/login', 'login')->name('login');
 Route::post('/login', LoginController::class)->name('login-post');
 
-Route::view('/register', 'register')->name('register');
-Route::post('/register', RegisterController::class)->name('register-post');
-
 Route::middleware('auth')->group(function () {
     Route::redirect('/', 'live');
     Route::get('/live', LiveController::class)
@@ -75,6 +72,9 @@ Route::middleware('auth:admin')->group(function () {
 
         return ['token' => $token->plainTextToken];
     });
+     
+    Route::view('/register', 'register')->name('register');
+    Route::post('/register', RegisterController::class)->name('register-post');
 });
 
 /**
